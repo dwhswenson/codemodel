1501d73914584e96a3e1c4f3201e4f62e45d3b11
David W.H. Swenson <dwhs@hyperblazer.net>
2019-11-03 12:41:18 +0100

    docstrings and tests for imports

--- codemodel/imports.py
+++ codemodel/imports.py
@@ -1,6 +1,13 @@
 import ast
 
 def validate_imports(imports):
+    """Validate that the given list of imports only includes imports.
+
+    Parameters
+    ----------
+    imports : list of str
+        string that should be import statements
+    """
     if isinstance(imports, str):
         imports = [imports]
     for line in imports:
@@ -9,10 +16,14 @@ def validate_imports(imports):
             raise RuntimeError("Non-import statement in imports: "
                                + str(line))
 
+
 def _name_or_asname(alias):
+    """Take alias node from AST and return the name bound to import"""
     return alias.asname if alias.asname else alias.name
 
+
 class _FindImportNames(ast.NodeVisitor):
+    # identify all the import names, map them to canonical names
     def __init__(self):
         super().__init__()
         self.import_names = {}
@@ -28,7 +39,22 @@ class _FindImportNames(ast.NodeVisitor):
         self.import_names.update(dct)
         self.generic_visit(node)
 
+
 def import_names(imports):
+    """Link import names to modules for import lines of code.
+
+    Note that this doesn't handle * imports, e.g., ``from foo import *``.
+
+    Parameters
+    ----------
+    imports : list of str
+        lines of code representing imports
+
+    Returns
+    -------
+    dict :
+        mapping of name (used in source) to an importable name of the module
+    """
     if isinstance(imports, str):
         imports = [imports]
     validate_imports(imports)
