af7750375567d5518c40f6dfeadfd1e97c9c2965
David W.H. Swenson <dwhs@hyperblazer.net>
2019-11-06 15:36:11 +0100

    docstrings in asttools

--- codemodel/asttools.py
+++ codemodel/asttools.py
@@ -56,7 +56,25 @@ def organize_parameter_names(func):
     """Organize the parameter names by how they will be displayed.
 
     This prefers to use keywords whenever possible (compared to
-    inspect.BoundParameters, which seems to prefer positional arguments).
+    inspect.BoundParameters, which prefers positional arguments).
+
+    Parameters
+    ----------
+    func : callable
+        callable to analyze
+
+    Returns
+    -------
+    as_pos : list of str
+        names of parameters to treat as positional
+    var_pos : str
+        parameter name for variadic positional arguments (usually ``args``),
+        or None if no variadic positional arguments
+    as_kw : list of str
+        names of parameters to treat as keywords
+    var_kw : str
+        parameter name for variadic keyword arguments (usually ``kwargs``),
+        or None if no variadic keywork arguments
     """
     sig = inspect.signature(func)
     param_kinds = collections.defaultdict(list)
@@ -89,6 +107,22 @@ def organize_parameter_names(func):
     return as_pos, var_pos, as_kw, var_kw
 
 def get_args_kwargs(func, param_dict):
+    """Get *args and **kwargs appropriate to do func(*args, **kwargs).
+
+    This uses our preference for keywords over positional arguments.
+
+    Parameters
+    ----------
+    func : callable
+    param_dict : dict
+        mapping of the string parameter name to the associated value, where
+        the parameter name is as given in the func's definition.
+
+    Returns
+    -------
+    args, kwargs : tuple of list, dict
+        appropriate results for func(*args, **kwargs)
+    """
     as_pos, var_pos, as_kw, var_kw = organize_parameter_names(func)
     args = [param_dict[p] for p in as_pos]
     if var_pos:
@@ -100,24 +134,53 @@ def get_args_kwargs(func, param_dict):
 
 
 def deindented_source(src):
+    """De-indent source if all lines indented.
+
+    This is necessary before parsing with ast.parse to avoid "unexpected
+    indent" syntax errors if the function is not module-scope in its
+    original implementation (e.g., staticmethods encapsulated in classes).
+
+    Parameters
+    ----------
+    src : str
+        input source
+
+    Returns
+    -------
+    str :
+        de-indented source; the first character of at least one line is
+        non-whitespace, and all other lines are deindented by the same
+    """
     lines = src.splitlines()
     min_chars_whitespace = float("inf")
     for line in lines:
         if line:
             idx = 0
+            # we're Python 3, so we assume you're not mixing tabs and spaces
             while idx < min_chars_whitespace and line[idx] in [" ", '\t']:
                 idx += 1
 
             min_chars_whitespace = min(idx, min_chars_whitespace)
 
-    print(min_chars_whitespace)
     lines = [line[min_chars_whitespace:] for line in lines]
     src = "\n".join(lines)
     return src
 
 
 def is_return_dict_function(func):
-    """Check whether a function returns a dictionary"""
+    """Check whether a function returns a dictionary
+
+    This assumes that the function is single-return.
+
+    Parameters
+    ----------
+    func : callable
+        a single-return functions
+
+    Returns
+    bool :
+        whether the function returns a dict
+    """
     tree = ast.parse(deindented_source(inspect.getsource(func)))
     func_body = tree.body[0].body
     for node in func_body:
@@ -125,3 +188,20 @@ def is_return_dict_function(func):
             return True
 
     return False
+
+
+def return_dict_func_to_ast_body(func):
+    """
+    Take a function that returns a dict and prepares it for the body.
+    """
+    pass
+
+def instantiation_func_to_ast(func):
+    """
+    """
+    pass
+
+def create_call_ast(func, param_dict, assign=None, prefix=None):
+    """
+    """
+    pass
