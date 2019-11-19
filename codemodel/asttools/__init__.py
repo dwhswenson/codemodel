94be56c7ba5fc5f14d61ee14dac0704112484f96
David W.H. Swenson <dwhs@hyperblazer.net>
2019-11-19 16:29:08 +0100

    imports => asttools.imports

--- codemodel/asttools/__init__.py
+++ codemodel/asttools/__init__.py
@@ -1,10 +1,12 @@
+from .imports import validate_imports, import_names
+
 from .function_handling import (
     organize_parameter_names, get_args_kwargs, deindented_source,
     func_to_body_tree
 )
 from .validators import (
     ScopeTracker, ScopeLister, count_returns,
-    find_required_inputs, validate_return_dict, is_return_dict_func
+    validate_return_dict, is_return_dict_func
 )
 from .rewriters import (
     replace_ast_names, return_to_assign, global_return_dict_to_assign,
